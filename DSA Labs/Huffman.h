a75626d2b44cefe9e2febfae530c79b420dabb28
Terry Ellison <ellisonterry4@gmail.com>
2022-08-28 00:42:50 -0400

    stuck on huffman

--- DSA Labs/Huffman.h
+++ DSA Labs/Huffman.h
@@ -39,17 +39,17 @@ NOTE: If the unit test is not on, that code will not be compiled!
 */
 
 // Main toggle
-#define LAB_8 0
+#define LAB_8 1
 
 // Individual unit test toggles
-#define HUFFMAN_CTOR					0
-#define HUFFMAN_GENERATE_FREQUENCY		0
-#define HUFFMAN_GENERATE_LEAFLIST		0
-#define HUFFMAN_GENERATE_TREE			0
-#define HUFFMAN_CLEAR_TREE				0
+#define HUFFMAN_CTOR					1
+#define HUFFMAN_GENERATE_FREQUENCY		1
+#define HUFFMAN_GENERATE_LEAFLIST		1
+#define HUFFMAN_GENERATE_TREE			1
+#define HUFFMAN_CLEAR_TREE				1
 #define HUFFMAN_DTOR					0
-#define HUFFMAN_GENERATE_ENCODING		0
-#define HUFFMAN_COMPRESS				0
+#define HUFFMAN_GENERATE_ENCODING		1
+#define HUFFMAN_COMPRESS				1
 #define HUFFMAN_DECOMPRESS				0
 
 // Wraps up Huffman compression algorithm
@@ -102,7 +102,7 @@ class Huffman {
 	// Data members
 	// NOTE: Values set to 1/-1 for unit test purposes
 	std::string mFileName = "-1";						// The name of the file to open for read/write
-	unsigned int mFrequencyTable[256] = { 1 };			// The frequency of each character in the file (the index in the array is the char's ASCII value)
+	unsigned int mFrequencyTable[257] = { 1 };			// The frequency of each character in the file (the index in the array is the char's ASCII value)
 	std::vector<HuffNode*> mLeafList;						// Contains all of the dynamically allocated leaf nodes
 	HuffNode* mRoot = reinterpret_cast<HuffNode*>(-1);	// The root of the huffman tree (set in GenerateTree)
 	std::vector<bool> mEncodingTable[256];					// Contains all of the bit-codes used to compress/decompress a file
@@ -112,17 +112,20 @@ class Huffman {
 	// In:	_fileName		The name of the file to open
 	Huffman(const std::string& _fileName) {
 		// 1. Assign the data members the values from the parameters
+		this->mFileName = _fileName;
 
 		// 2. Zero out the frequency table
+		std::memset(this->mFrequencyTable, 0, sizeof(this->mFrequencyTable));
 
 		// 3. Set the root to null
-
+		this->mRoot = NULL;
 	}
 
 	// Destructor
 	//		Needs to clean up any left-over dynamic memory in tree
 	~Huffman() {
 		// 1. Clear out the tree
+		this->ClearTree();
 
 	}
 
@@ -135,12 +138,23 @@ class Huffman {
 	void GenerateFrequencyTable() {
 
 		// 1. Open the file in binary mode (using a standard ifstream)
+		unsigned char buffer = 0;
+		std::ifstream inputFile(this->mFileName, std::ios::in | std::ios::binary);
+
 
 		// 2. Read the file one byte at a time, and increment the corresponding index
+		if (inputFile.is_open())
+		{
+			while (inputFile.read((char*)&buffer, sizeof(unsigned char)))
+			{
+				unsigned int num = buffer;
+				++this->mFrequencyTable[num];
+				++this->mFrequencyTable[256];
+			}
+			inputFile.close();
+		}
 
 		// 3. Close the file when complete
-
-
 	}
 
 	// Generate the leaf list for the Huffman algorithm (used in READ AND WRITE)
@@ -149,6 +163,13 @@ class Huffman {
 	void GenerateLeafList() {
 		// 1. Iterate through the frequency table and dynamically create a leaf node for each non-0
 		// frequency. Add it to the mLeafList vector.
+		for (short i = 0; i <= 255; ++i)
+		{
+			if (this->mFrequencyTable[i] != 0)
+			{
+				this->mLeafList.push_back(new HuffNode(i, this->mFrequencyTable[i]));
+			}
+		}
 
 	}
 
@@ -157,9 +178,15 @@ class Huffman {
 		// 1. Create the priority queue
 		//              This will be storing HuffNode*'s
 		//              in a vector, and will be using the HuffCompare for comparison
+		std::priority_queue<HuffNode*,std::vector<HuffNode*>, HuffCompare> queue;
 
+		
 		// 2. Add in all data from your leaf list
-
+		for (auto it = this->mLeafList.begin(); it != this->mLeafList.end(); ++it)
+		{
+			queue.push(*it);
+		}
+		
 		// 3. Enter the tree generation algorithm
 		//              While the queue has more than 1 node
 		//                      Store the top two nodes into some temporary pointers and pop them
@@ -167,8 +194,21 @@ class Huffman {
 		//                      Set the parent value to -1, and frequency to the sum of its children
 		//                      Set the 1st and 2nd node's parent to the new node you created
 		//                      Insert new node into queue
+		while (queue.size() > 1)
+		{
+			HuffNode* left = queue.top();
+			queue.pop();
+			HuffNode* right = queue.top();
+			queue.pop();
 
+			HuffNode* parent = new HuffNode(-1, (left->freq + right->freq), left, right);
+			left->parent = parent, right->parent = parent;
+
+			queue.push(parent);
+		}
 		// 4. Set the root of the tree (this will be the only node in the queue)
+		this->mRoot = queue.top();
+		queue.pop();
 
 	}
 
@@ -182,12 +222,35 @@ class Huffman {
 		//			and a 1 if you passed through a right
 		//			Once you hit the root node, reverse the values in the vector
 
+		for (auto it = this->mLeafList.begin(); it != this->mLeafList.end(); ++it)
+		{
+			HuffNode* temp = *it;
+			std::vector<bool>* bitCode = &this->mEncodingTable[temp->value];
+
+			while (temp != this->mRoot)
+			{
+				if (temp == temp->parent->left)
+				{
+					bitCode->push_back(false);
+				}
+				else
+				{
+					bitCode->push_back(true);
+				}
+
+				temp = temp->parent;
+			}
+
+			std::reverse(bitCode->begin(), bitCode->end());
+		}
+
 	}
 
 	// Clear the tree of all dynamic memory (by using the helper function)
 	void ClearTree() {
 		// 1. Call the helper function with the root and then set it back to null
-
+		this->ClearTree(this->mRoot);
+		this->mRoot = NULL;
 	}
 
 	// Clear the tree of all dynamic memory (recursive helper function)
@@ -197,7 +260,21 @@ class Huffman {
 	// Note:	This will be a recursive function that does a post-order deletion
 	void ClearTree(HuffNode* _curr) {
 		// 1. Implement this method
-
+		if (_curr != NULL)
+		{
+			if (_curr->left != NULL)
+			{
+				this->ClearTree(_curr->left);
+				_curr->left = NULL;
+			}
+			if (_curr->right != NULL)
+			{
+				this->ClearTree(_curr->right);
+				_curr->right = NULL;
+			}
+			delete _curr;
+			_curr = NULL;
+		}
 	}
 
 	// Write a Huffman compressed file to disk
@@ -207,10 +284,27 @@ class Huffman {
 	// Note: You will use most of your other functionality to complete this function
 	void Compress(const char* _outputFile) {
 		// 1. Create the frequency table, leaf list, tree, and encoding table (in this order)
+		this->GenerateFrequencyTable();
+		this->GenerateLeafList();
+		this->GenerateTree();
+		this->GenerateEncodingTable();
 
 		// 2. Create a BitOStream and supply it the huffman header
-
+		BitOfstream outputStream(_outputFile, (const char*)mFrequencyTable, mFrequencyTable[256]);
+		
 		// 3. Open the input file in binary mode with a standard ifstream
+
+		std::ifstream inputFile(this->mFileName, std::ios::binary);
+		unsigned char buffer = 0;
+
+		while (inputFile.read((char*)&buffer, sizeof(unsigned char)))
+		{
+			auto bitCode = this->mEncodingTable[(unsigned int)buffer];
+			outputStream << bitCode;
+		}
+
+		inputFile.close();
+		//outputStream.Close();
 
 		// 4. Start the compression process.   (You can read the whole file into a buffer first if you want)
 		//		For each character in the original file, write out the bit-code from the encoding table
@@ -242,5 +336,7 @@ class Huffman {
 		// 8. Clean up the dynamic memory by clearing the tree
 
 	}
+
+
 };
 
